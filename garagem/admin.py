b67f4a5a31882120380505633706069c1c19cf35
MarceloHarbs <mharbs614@gmail.com>
2023-03-28 16:33:30 -0300

    Criação model Cores

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Acessorio, Categoria, Marca
+from garagem.models import Acessorio, Categoria, Cor, Marca
 
 admin.site.register(Acessorio)
 admin.site.register(Categoria)
+admin.site.register(Cor)
 admin.site.register(Marca)
 
--- garagem/models.py
+++ garagem/models.py
@@ -18,3 +18,13 @@ class Acessorio(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Cores"
+    
