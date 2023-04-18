bbecdee7e416c8ab2bc469a6ceb8a512284e787b
MarceloHarbs <mharbs614@gmail.com>
2023-04-18 14:39:43 -0300

    Api Marca

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import CategoriaViewSet
+from garagem.views import CategoriaViewSet, MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
-
+router.register(r"marca", MarcaViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
