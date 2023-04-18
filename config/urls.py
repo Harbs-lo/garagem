cc70f4ade3482ca2b5d28af944decd67bcfe7b49
MarceloHarbs <mharbs614@gmail.com>
2023-04-18 14:45:46 -0300

    Api Acessorio

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,10 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import CategoriaViewSet, CorViewSet, MarcaViewSet
+from garagem.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
 
 router = DefaultRouter()
+router.register(r"acessorio", AcessorioViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"cor", CorViewSet)
 router.register(r"marca", MarcaViewSet)
