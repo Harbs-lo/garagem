fcee5e4aa57a56bf79b500f04241a214958e9d43
MarceloHarbs <mharbs614@gmail.com>
2023-08-04 11:02:13 -0300

    inicio att 2 sem

--- garagem/models.py
+++ garagem/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.nome.upper()
+    
+    class Meta:
+        verbose_name_plural = "Marcas"
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
@@ -23,23 +26,35 @@ class Acessorio(models.Model):
         verbose_name = "Acessório"
     
 class Cor(models.Model):
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=50)
 
     def __str__(self):
         return self.descricao
     
     class Meta:
         verbose_name_plural = "Cores"
-     
-class Veiculo(models.Model):
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
-    ano = models.IntegerField(null=True, blank=True)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="modelos")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="modelos")
 
     def __str__(self):
-        return f"{self.marca} {self.categoria} {self.cor} {self.ano}"
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = "Modelos"
+
+class Veiculo(models.Model):
+    descricao = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    ano = models.IntegerField(null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
+
+    def __str__(self):
+        return f"{self.modelo} {self.descricao}"
     
     class Meta:
         verbose_name = "Veículo"
